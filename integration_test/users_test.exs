27f250bce2509a7a0e1d5cf955e3327d35aa231d
Skyler Nesheim <skylernesheim@gmail.com>
2014-08-21 18:23:45 -0500

    refactor integration tests for users

--- integration_test/users_test.exs
+++ integration_test/users_test.exs
@@ -4,21 +4,19 @@ defmodule UsersIntegrationTest do
   test "should get basic user info" do
     user_response = Dwolla.Users.get("skyler@dwolla.com", Dwolla.Client.new)
 
-    success = Dict.fetch!(user_response, "Success")
     user = Dict.fetch!(user_response, "Response")
 
-    assert true == success
-    assert "Skyler Nesheim" == Dict.fetch!(user, "Name")
-    assert "812-443-3023" == Dict.fetch!(user, "Id")
+    assert true == user_response |> Dict.fetch!("Success")
+    assert "Skyler Nesheim" == user |> Dict.fetch!("Name")
+    assert "812-443-3023" == user |> Dict.fetch!("Id")
   end
   
   test "should get account info with oauth token" do
     user_response = Dwolla.Users.account(Dwolla.Client.new)
     
-    success = Dict.fetch!(user_response, "Success")
     user = Dict.fetch!(user_response, "Response")
 
-    assert true == success
+    assert true == user_response |> Dict.fetch!("Success")
     assert "Skyler Nesheim" == Dict.fetch!(user, "Name")
     assert "812-443-3023" == Dict.fetch!(user, "Id")
     assert "Des Moines" == Dict.fetch!(user, "City")
@@ -27,16 +25,13 @@ defmodule UsersIntegrationTest do
 
   test "should get avatar given dwolla id" do
     response = Dwolla.Users.avatar("812.443.3023").body 
-      
+    
     assert nil != response
   end
 
   test "should get nearby" do 
-    response = Dwolla.Users.nearby(0,0,Dwolla.Client.new)
-
-    success = Dict.fetch!(response, "Success")
-
-    assert true == success
+    assert true  = Dwolla.Users.nearby(0,0,Dwolla.Client.new)
+      |> Dict.fetch!("Success")
   end
 
 end
