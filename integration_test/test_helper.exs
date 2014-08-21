818baeed29f043d6ed1cd0692adb15d94bf6acf7
Skyler Nesheim <skylernesheim@gmail.com>
2014-08-20 22:42:21 -0500

    refactor integration test helper

--- integration_test/test_helper.exs
+++ integration_test/test_helper.exs
@@ -3,13 +3,10 @@ ExUnit.start
 :application.set_env(:dwolla_elixir, :url, "https://uat.dwolla.com/oauth/rest/")
 
 defmodule Dwolla.IntegrationTest do
-  def get_pin do
-    System.get_env("PIN")
-  end
+  
+  def get_pin, do: System.get_env("PIN")
 
-  def get_merchant_token do
-    System.get_env("MERCHANT_TOKEN")
-  end
+  def get_merchant_token, do: System.get_env("MERCHANT_TOKEN")
 
 end
 
