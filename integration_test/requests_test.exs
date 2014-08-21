08df83d5c8df329bf1c3c7b7a03c4ec9e52268a6
Skyler Nesheim <skylernesheim@gmail.com>
2014-08-20 22:40:07 -0500

    refactor requests integration test

--- integration_test/requests_test.exs
+++ integration_test/requests_test.exs
@@ -4,17 +4,12 @@ defmodule RequestsIntegrationTest do
   import Dwolla.IntegrationTest
 
   test "should request money" do
-    response = request()
-    
-    success = Dict.fetch!(response, "Success")
-    id = Dict.fetch!(response, "Response")
-
-    assert true == success
-    assert 0 < id
+    assert true == request()
+    |> Dict.fetch!("Success")
   end
 
   test "should fulfill request" do
-    id = Dict.fetch!(request(), "Response")
+    id = request() |> Dict.fetch!("Response")
 
     response = Dwolla.Requests.fulfill(
       id,
@@ -22,15 +17,12 @@ defmodule RequestsIntegrationTest do
        amount: ".01"],
       Dwolla.Client.client(token: get_merchant_token())) 
 
-    success = Dict.fetch!(response, "Success")
     details = Dict.fetch!(response, "Response")
-    source = Dict.fetch!(details, "Source")
-    dest = Dict.fetch!(details, "Destination")
 
-    assert true == success
+    assert true == response |> Dict.fetch!("Success")
     assert 0.01 == Dict.fetch!(details, "Amount")
-    assert "812-201-0130" == Dict.fetch!(source,"Id")
-    assert "812-443-3023" == Dict.fetch!(dest,"Id")
+    assert "812-201-0130" == details |> Dict.fetch!("Source") |> Dict.fetch!("Id")
+    assert "812-443-3023" == details |> Dict.fetch!("Destination") |> Dict.fetch!("Id")
   end
 
   test "should list pending requests" do
@@ -43,41 +35,33 @@ defmodule RequestsIntegrationTest do
       %{"start_date" => "2014-04-01",
         "limit" => "3"})
 
-    success = Dict.fetch!(response, "Success")
     details = Dict.fetch!(response, "Response")
 
-    assert true == success
-    assert 3 == Enum.count details
-    assert true = Enum.all? details, fn(x) -> Dict.fetch!(x, "Status") == "Pending" end 
+    assert true == response |> Dict.fetch!("Success")
+    assert 3 == details |> Enum.count 
+    assert true = details |> Enum.all?(fn(x) -> Dict.fetch!(x, "Status") == "Pending" end) 
 
   end
   
   test "should get requests by id" do 
-    id = Dict.fetch!(request(), "Response")
+    id = request() |> Dict.fetch!("Response")
 
     response = Dwolla.Requests.get_by_id(id, Dwolla.Client.new)
 
-    success = Dict.fetch!(response, "Success")
-    returned_id = Dict.fetch!(Dict.fetch!(response, "Response"), "Id")
-
-    assert true == success
-    assert returned_id == id
+    assert true == response |> Dict.fetch!("Success")
+    assert id  == response |> Dict.fetch!("Response") |> Dict.fetch!("Id")
   end
 
   test "should cancel request" do 
-    id = Dict.fetch!(request(), "Response")
-
-    response = Dwolla.Requests.cancel(id, Dwolla.Client.new)
-
-    success = Dict.fetch!(response, "Success")
-
-    assert true == success
+    assert true == request() 
+      |> Dict.fetch!("Response")
+      |> Dwolla.Requests.cancel(Dwolla.Client.new)
+      |> Dict.fetch!("Success")
   end
 
   defp request, do: Dwolla.Requests.request(
       [sourceId: "812-201-0130",
        amount: ".01"], Dwolla.Client.new)
-
 
 end 
 
